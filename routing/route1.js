58c163d01f892ddf68a0b0497fb5ae2dcdc34f2e
Tawin Ch <6322775007@g.siit.tu.ac.th>
2023-11-16 09:08:54 +0700

    yum

--- routing/route1.js
+++ routing/route1.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path =  require('path');
 
+const {DynamoDBClient} = require('aws-sdk');
 
 router.get("/",(req,res)=>{
     res.render('login');
