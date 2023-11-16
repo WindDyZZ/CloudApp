8421dda3b3960b3ebb4cdf54f7fc049f0bdff721
Tawin Ch <6322775007@g.siit.tu.ac.th>
2023-11-16 14:44:57 +0700

    config route

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 const path = require('path');
 const router = require('./routing/route1');
 
+
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 app.use(router);
 app.use(express.static(path.join(__dirname,'public')))
+
 
 app.listen(80,()=>{
     console.log('Start server');
