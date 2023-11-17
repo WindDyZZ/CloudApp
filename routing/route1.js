7e4ef4a8fa2ef272a505556dfa2e74a7d2bd5258
Napatsorn1559 <6322771559@g.siit.tu.ac.th>
2023-11-17 17:06:15 +0700

    update history.ejs, style.css and route1.js

--- routing/route1.js
+++ routing/route1.js
@@ -1,45 +1,93 @@
 const express = require('express');
+const { stat } = require('fs');
 const router = express.Router();
-const path =  require('path');
+const path = require('path');
 
 
-router.get("/",(req,res)=>{
+router.get("/", (req, res) => {
     res.render('login');
 })
 
-router.get("/register",(req,res)=>{
+router.get("/register", (req, res) => {
     res.render('register');
 })
 
-router.get("/index",(req,res)=>{
+router.get("/index", (req, res) => {
     const name = "Win";
-    res.render('index.ejs',{name:name});
+    res.render('index.ejs', { name: name });
 })
 
-router.get("/home",(req,res)=>{
+router.get("/home", (req, res) => {
     const num = 10;
     const pnum = 2;
-    res.render('home.ejs',{num: num, pnum:pnum});
+    res.render('home.ejs', { num: num, pnum: pnum });
 })
 
-router.get("/profile",(req,res)=>{
+router.get("/profile", (req, res) => {
     res.render('profile');
 })
 
-router.get("/myshop",(req,res)=>{
+router.get("/myshop", (req, res) => {
     res.render('myshop');
 })
 
-router.get("/add_product",(req,res)=>{
+router.get("/add_product", (req, res) => {
     res.render('add_product');
 })
 
-router.get("/cart",(req,res)=>{
+router.get("/cart", (req, res) => {
     res.render('cart.ejs')
 })
 
-router.get("/history",(req,res)=>{
-    res.render('history.ejs')
+
+
+router.get("/history", (req, res) => {
+    const status = req.query.stt;
+    const user = {
+        firstName: 'Tim',
+        lastName: 'Cook',
+    }
+
+    if (status === 'purchased') {
+        var product = 
+        [{
+            name: 'purchased product ',
+            detail: 'product detail ...',
+            tp_cost: 40,
+            total_price: 200
+        },
+        {
+            name: 'purchased product 2 ',
+            detail: 'product detail ...',
+            tp_cost: 40,
+            total_price: 200
+        },
+    ]
+        
+    }else if (status === 'cancel'){
+        var product = [{
+            name: 'cancel product ',
+            detail: 'product detail ...',
+            tp_cost: 40,
+            total_price: 200
+        }]
+    }else{
+        var product = 
+        [{
+            name: 'refund product ',
+            detail: 'product detail ...',
+            tp_cost: 40,
+            total_price: 200
+        },
+        {
+            name: 'refund product 2 ',
+            detail: 'product detail ...',
+            tp_cost: 40,
+            total_price: 200
+        },
+    ]
+    }
+    res.render('history.ejs', {product:product})
 })
 
 
