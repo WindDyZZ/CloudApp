aebf97f14d810ef39c2fb893be5bea8a523d4412
Napatsorn1559 <6322771559@g.siit.tu.ac.th>
2023-11-20 03:36:45 +0700

    new branch and enable login , create account

--- routing/route1.js
+++ routing/route1.js
@@ -4,167 +4,177 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const AWS = require('aws-sdk');
 const {
-  DynamoDBDocument, GetCommand, PutCommand, ScanCommand
+    DynamoDBDocument, GetCommand, PutCommand, ScanCommand
 } = require('@aws-sdk/lib-dynamodb');
 const {
-  DynamoDBClient, 
+    DynamoDBClient,
 } = require('@aws-sdk/client-dynamodb');
-const client = new DynamoDBClient({region:'us-east-1'});
+const client = new DynamoDBClient({ region: 'us-east-1' });
 const dynamoDB = DynamoDBDocument.from(client);
-router.use(bodyParser.urlencoded({extended:false}));
+router.use(bodyParser.urlencoded({ extended: false }));
 AWS.config.update({
-  accessKeyId:'ASIA3KOLCAFPAEBODSGK',
-  secretAccessKey:'UwjvnAHVjuP6q6tB7o22aeNIpOZRwmimUN6BAYtE',
-  region:'us-east-1',
+    accessKeyId: 'ASIAYFXVHD42YHROOPDP',
+    secretAccessKey: '3JoR9nTOExjnO6AIzx2eIxqxuAqTAd4ddaJJYQe1',
+    region: 'us-east-1',
 
 });
 // assign global var username
 let cur_username = '';
+let cur_userObj = {
+    password: 'test1234',
+    history: ['orderID1', 'orderID2'],
+    lastName: 'Doe',
+    username: 'johndoedoe',
+    address: '11/12 Somewhere inYourHeart Bangkok 12123',
+    email: 'tester@gmail.com',
+    cart: ['', 'productID1', 'productID2'],
+    firstName: 'John'
+}
 
-router.get('/',(req,res)=>{
-  res.render('login');
+router.get('/', (req, res) => {
+    res.render('login');
 })
 
 router.post("/", async (req, res) => {
-  const username = req.body.login_username.toLowerCase();
-  const password = req.body.login_password;
+    const username = req.body.login_username.toLowerCase();
+    const password = req.body.login_password;
 
-  console.log('Entered username:', username);
+    console.log('Entered username:', username);
 
-  const get_data = async () => {
-      const command = new GetCommand({
-          TableName: "Users",
-          Key: {
-              'email': username,
-          },
-      });
+    const get_data = async () => {
+        const command = new GetCommand({
+            TableName: "user",
+            Key: {
+                'email': username,
+            },
+        });
 
-      try {
-          const response = await dynamoDB.send(command);
-          return response;
-      } catch (error) {
-          console.error('Error retrieving item from DynamoDB:', error);
-          return null;
-      }
-  };
+        try {
+            const response = await dynamoDB.send(command);
+            return response;
+        } catch (error) {
+            console.error('Error retrieving item from DynamoDB:', error);
+            return null;
+        }
+    };
 
-  const data = await get_data();
+    const data = await get_data();
 
-  if (data && data.Item) {
-      console.log('Stored password:', data.Item.password);
-      if (password === data.Item.password.toLowerCase()) {
-          cur_username = username;
-          res.redirect('/home');
-      } else {
-          res.render('login', { 'wrong_pass': true });
-      }
-  } else {
-      console.log('User not found in DynamoDB.');
-      res.render('login', { 'wrong_pass': true });
-  }
+    if (data && data.Item) {
+        console.log('Stored password:', data.Item.password);
+        if (password === data.Item.password.toLowerCase()) {
+            cur_username = username;
+            cur_userObj = data.Item;
+            console.log(cur_userObj);
+            res.redirect('/home');
+        } else {
+            res.render('login', { 'wrong_pass': true });
+        }
+    } else {
+        console.log('User not found in DynamoDB.');
+        res.render('login', { 'wrong_pass': true });
+    }
 });
 router.get("/register", (req, res) => {
     res.render('register');
 })
 
 router.post("/register", async (req, res) => {
-  const username = req.body.register_username.toLowerCase();
-  const email = req.body.register_Email;
-  const password = req.body.login_password;
-  const fname = req.body.register_firstName;
-  const lname = req.body.register_lastName;
+    const username = req.body.register_username.toLowerCase();
+    const email = req.body.register_Email;
+    const password = req.body.register_Password;
+    const fname = req.body.register_firstName;
+    const lname = req.body.register_lastName;
+    const address = req.body.register_address;
 
-  const params_email = {
-      TableName: 'Users',
-      Key: {
-          email: email,
-      },
-  };
+    const params_email = {
+        TableName: 'user',
+        Key: {
+            email: email,
+        },
+    };
 
-  const command_email = new GetCommand(params_email);
+    const command_email = new GetCommand(params_email);
 
-  try {
-      const responseEmail = await dynamoDB.send(command_email);
+    try {
+        const responseEmail = await dynamoDB.send(command_email);
 
-      if (responseEmail && responseEmail.Item) {
-          res.render('register', { 'existed_email': true });
-      } else {
-          const params_username = {
-              TableName: 'Users',
-              Key: {
-                'email': email,
-              },
-          };
+        if (responseEmail && responseEmail.Item) {
+            res.render('register', { 'existed_email': true });
+        } else {
+            const params_username = {
+                TableName: 'user',
+                Key: {
+                    'email': email,
+                },
+            };
 
-          const command_username = new GetCommand(params_username);
-          const responseUsername = await dynamoDB.send(command_username);
+            const command_username = new GetCommand(params_username);
+            const responseUsername = await dynamoDB.send(command_username);
 
-          if (responseUsername && responseUsername.Item) {
-              res.render('register', { 'existed_username': true });
-          } else {
-              // Add a new user to the DynamoDB table
-              const input = {
-                  TableName: "Users",
-                  Item: {
-                      email: email,
-                      username: username,
-                      password: password,
-                      firstName: fname,
-                      lastName: lname,
-                  },
-              };
+            if (responseUsername && responseUsername.Item) {
+                res.render('register', { 'existed_username': true });
+            } else {
+                // Add a new user to the DynamoDB table
+                const input = {
+                    TableName: "user",
+                    Item: {
+                        email: email,
+                        username: username,
+                        password: password,
+                        firstName: fname,
+                        lastName: lname,
+                        address: address
+                    },
+                };
 
-              const putCommand = new PutCommand(input);
-              await dynamoDB.send(putCommand);
+                const putCommand = new PutCommand(input);
+                await dynamoDB.send(putCommand);
 
-              res.render('register', { 'success': true });
-          }
-      }
-  } catch (error) {
-      console.error('Error getting item from DynamoDB:', error);
-      res.render('register', { 'error1': true });
-  }
+                res.render('login', { 'success': true });
+            }
+        }
+    } catch (error) {
+        console.error('Error getting item from DynamoDB:', error);
+        res.render('register', { 'error1': true });
+    }
 });
 
 
-router.get("/index", (req, res) => {
-    const name = "Win";
-    res.render('index.ejs', { name: name });
-})
 
 router.get("/home", (req, res) => {
-  const username = cur_username;
+    const username = cur_username;
 
-  // Fetch user data from DynamoDB using the username
-  const getUserData = async (username) => {
-      const command = new GetCommand({
-          TableName: "Users",
-          Key: {
-              'email': username,
-          },
-      });
+    // Fetch user data from DynamoDB using the username
+    const getUserData = async (username) => {
+        const command = new GetCommand({
+            TableName: "user",
+            Key: {
+                'email': username,
+            },
+        });
 
-      try {
-          const response = await dynamoDB.send(command);
-          return response.Item; // Return the user data
-      } catch (error) {
-          console.error('Error retrieving user data from DynamoDB:', error);
-          return null;
-      }
-  };
+        try {
+            const response = await dynamoDB.send(command);
+            return response.Item; // Return the user data
+        } catch (error) {
+            console.error('Error retrieving user data from DynamoDB:', error);
+            return null;
+        }
+    };
 
-  getUserData(username)
-      .then(userData => {
-          res.render('home.ejs', { 'username': cur_username, 'Item': userData });
-      })
-      .catch(error => {
-          console.error('Error fetching user data:', error);
-          res.render('home.ejs', { 'username': cur_username, 'Item': null });
-      });
+    getUserData(username)
+        .then(userData => {
+            res.render('home.ejs', { 'username': cur_username, 'Item': userData });
+        })
+        .catch(error => {
+            console.error('Error fetching user data:', error);
+            res.render('home.ejs', { 'username': cur_username, 'Item': null });
+        });
 });
 
 router.get("/profile", (req, res) => {
-    res.render('profile',{'username':cur_username});
+    res.render('profile', { 'username': cur_username, 'userData': cur_userObj });
 })
 
 router.get("/myshop", (req, res) => {
@@ -193,50 +203,50 @@ router.get("/history", (req, res) => {
     }
 
     if (status === 'purchased') {
-        var product = 
-        [{
-            name: 'Purchased Product ',
-            detail: 'product detail ...',
-            tp_cost: 40,
-            total_price: 200
-        },
-        {
-            name: 'Purchased Product 2 ',
-            detail: 'product detail ...',
-            tp_cost: 40,
-            total_price: 200
-        },
-    ]
-        
-    }else if (status === 'cancel'){
+        var product =
+            [{
+                name: 'Purchased Product ',
+                detail: 'product detail ...',
+                tp_cost: 40,
+                total_price: 200
+            },
+            {
+                name: 'Purchased Product 2 ',
+                detail: 'product detail ...',
+                tp_cost: 40,
+                total_price: 200
+            },
+            ]
+
+    } else if (status === 'cancel') {
         var product = [{
             name: 'Canceled Product ',
             detail: 'product detail ...',
             tp_cost: 40,
             total_price: 200
         }]
-    }else{
-        var product = 
-        [{
-            name: 'Refund Product ',
-            detail: 'product detail ...',
-            tp_cost: 40,
-            total_price: 200
-        },
-        {
-            name: 'Refund Product 2 ',
-            detail: 'product detail ...',
-            tp_cost: 40,
-            total_price: 200
-        },
-    ]
+    } else {
+        var product =
+            [{
+                name: 'Refund Product ',
+                detail: 'product detail ...',
+                tp_cost: 40,
+                total_price: 200
+            },
+            {
+                name: 'Refund Product 2 ',
+                detail: 'product detail ...',
+                tp_cost: 40,
+                total_price: 200
+            },
+            ]
     }
-    res.render('history.ejs', {product:product})
+    res.render('history.ejs', { product: product })
 })
 
-router.get('/logout', (req,res)=>{
-  cur_username = '';
-  res.redirect('/');
+router.get('/logout', (req, res) => {
+    cur_username = '';
+    res.redirect('/');
 })
 
 
