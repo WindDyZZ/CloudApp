7121157c9713155adb8aa2cc5cbbf415cb8f9c42
Kanyaphach <6322773068@g.siit.tu.ac.th>
2023-11-21 03:07:44 +0700

    update cart but still cannot show data from table

--- routing/route1.js
+++ routing/route1.js
@@ -3,14 +3,12 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const AWS = require('aws-sdk');
-// const { PutCommand } = require('@aws-sdk/client-s3');
 const { v4: uuidv4 } = require('uuid');
 const multer = require('multer');
 
 // Lib DB
 const {
-  DynamoDBDocument, GetCommand, PutCommand, ScanCommand, QueryCommand, DeleteCommand
-} = require('@aws-sdk/lib-dynamodb');
+  DynamoDBDocument, GetCommand, BatchGetCommand, PutCommand, ScanCommand, QueryCommand, DeleteCommand } = require('@aws-sdk/lib-dynamodb');
 
 // DB Client
 const {
@@ -199,94 +197,6 @@ router.post("/register", upload.single('profilePicture'), async (req, res) => {
 });
 
 
-// router.post("/register",upload.single('profilePicture'), async (req, res) => {
-//   const username = req.body.register_username.toLowerCase();
-//   const email = req.body.register_Email;
-//   const password = req.body.register_password;
-//   const fname = req.body.register_firstName;
-//   const lname = req.body.register_lastName;
-//   const address = req.body.register_address;
-
-//   const params_email = {
-//       TableName: 'Users',
-//       Key: {
-//           email: email,
-//       },
-//   };
-
-//   const command_email = new GetCommand(params_email);
-
-//   try {
-//       const responseEmail = await dynamoDB.send(command_email);
-
-//       if (responseEmail && responseEmail.Item) {
-//           res.render('register', { 'existed_email': true });
-//       } else {
-//           const params_username = {
-//             TableName: 'Users',
-//             FilterExpression: '#username =  :u' ,
-//             ExpressionAttributeNames:{'#username' : 'username'},
-//             ExpressionAttributeValues: {':u' : username}
-//           };
-
-//           const command_username = new ScanCommand(params_username);
-//           const responseUsername = await dynamoDB.send(command_username);
-
-//           if ( responseUsername.Count != 0) {
-//               res.render('register', { 'existed_username': true });
-//           } else {
-
-//               // const uploadS3 = {
-//               //   Bucket: 'web-otop',
-//               //   Key: `profile-pictures/${uuidv4()}.jpg`, 
-//               //   Body: req.file.buffer,
-//               //   ContentType: 'image/jpeg', 
-//               // };
-
-//               try{
-//                 const uploadResponse = await s3.send(new PutCommand(uploadS3));
-//                 const s3Url = `https://${params.Bucket}.s3.${s3.config.region}.amazonaws.com/${params.Key}`;
-//                 const input = {
-//                   TableName: "Users",
-//                   Item: {
-//                       email:    email,
-//                       username: username,
-//                       password: password,
-//                       firstName: fname,
-//                       lastName: lname,
-//                       address : address,
-//                       profile_pic: s3Url,
-//                   },
-//                 };
-
-//                 try{
-//                   const putCommand = new PutCommand(input);
-//                   await dynamoDB.send(putCommand);
-
-//                   cur_user = {
-//                     username:  email,
-//                     email:     username,
-//                     password:  password,
-//                     firstName: fname,
-//                     lastName:  lname,
-//                     address:   address,
-//                   };
-//                 }
-//                 catch(error){res.render('register',{'error2':true});}   
-//                 res.redirect('/home');
-//               }
-//               catch(error){
-//                 res.render('register',{'error3':true});
-//               }
-//           }
-//       }
-//   } catch (error) {
-//       console.error('Error getting item from DynamoDB:', error);
-//       res.render('register', { 'error1': true });
-//   }
-
-// });
-
 // Index -----------------------------------------------------
 
 router.get("/index", (req, res) => {
@@ -303,35 +213,7 @@ router.get("/home", (req, res) => {
 } else {
     res.render('home', {'cur_user': cur_user });
 }
-  // const username = cur_username;
 
-  // // Fetch user data from DynamoDB using the username
-  // const getUserData = async (username) => {
-  //     const command = new GetCommand({
-  //         TableName: "Users",
-  //         Key: {
-  //             'email': username,
-  //         },
-  //     });
-
-  //     try {
-  //         const response = await dynamoDB.send(command);
-  //         return response.Item; // Return the user data
-  //     } catch (error) {
-  //         console.error('Error retrieving user data from DynamoDB:', error);
-  //         return null;
-  //     }
-  // };
-
-  // getUserData(username)
-  //     .then(userData => {
-  //         res.render('home.ejs', { 'username': cur_username, 'Item': userData });
-  //     })
-  //     .catch(error => {
-  //         console.error('Error fetching user data:', error);
-  //         res.render('home.ejs', { 'username': cur_username, 'Item': null });
-  //     });
-  
 });
 
 
@@ -376,51 +258,6 @@ router.get("/myshop", async (req, res) => {
     res.render("myshop", { error: true });
   }
 });
-//My Shop Delete product--------------------------------------
-// router.post("/myshop/delete", async (req, res) => {
-//   try {
-//     if (!cur_user) {
-//       console.log("User not authenticated");
-//       return res.redirect("/"); // Adjust the login route as needed
-//     }
-
-//     const productIdsToDelete = req.body.productIds; // Use "productIds" instead of "productId"
-
-//     // Log the value of productIdsToDelete
-//     console.log('productIdsToDelete:', productIdsToDelete);
-
-//     // Ensure productIdsToDelete is an array
-//     const productIdsArray = [].concat(productIdsToDelete);
-
-//     for (const productIdToDelete of productIdsArray) {
-//       console.log('Deleting product with ID:', productIdToDelete);
-      
-//       // Split the productId into username and productID
-//       const [userID, productID] = productIdToDelete.split(',');
-
-//       console.log('Deleting product with userID and productID:', userID, productID);
-
-//       const deleteParams = {
-//         TableName: "Product",
-//         Key: {
-//           userID: { S: userID }, // Assuming userID is of type String
-//           productID: { S: productID }, // Assuming productID is of type String
-//         },
-//       };
-
-//       const deleteCommand = new DeleteCommand(deleteParams);
-//       await dynamoDB.send(deleteCommand);
-
-//       console.log('Product deleted successfully');
-//     }
-
-//     res.redirect("/myshop");
-//   } catch (error) {
-//     console.error("Error deleting product:", error);
-//     res.render("error", { error: true });
-//   }
-// });
-
 
 
 
@@ -475,12 +312,67 @@ router.post("/add_product", async (req, res) => {
 
 
 // Cart -----------------------------------------------------
-router.get("/cart", (req, res) => {
-    const Products = [
-        { product_name: "silk", image: "img/ex-product.png", price: 1000, description: "" },
-        { product_name: "silk", image: "img/ex-product.png", price: 1000, description: "" }
-    ];
+router.get("/cart", async (req, res) => {
+  try {
+    // Assuming cur_user contains the user information
+    if (!cur_user) {
+      console.log("User not authenticated");
+      return res.redirect("/");
+    }
+
+    const username = cur_user.email;
+
+    // Step 1: Query "Users" table to get the product IDs in the user's cart
+    const userQuery = {
+      TableName: "Users",
+      KeyConditionExpression: "email = :username",
+      ExpressionAttributeValues: {
+        ":username": username,
+      },
+    };
+
+    const userQueryCommand = new QueryCommand(userQuery);
+    const userQueryResult = await dynamoDB.send(userQueryCommand);
+
+    // Extract the cart attribute (assuming it's a string set)
+    const cartProductIds = userQueryResult.Items[0]?.cart?.values || [];
+
+    // Check if cartProductIds is an array
+    if (!Array.isArray(cartProductIds) || cartProductIds.length === 0) {
+      console.log("Cart is empty");
+      return res.render('cart.ejs', { emptyCart: true, Products: [] });
+    }
+
+    console.log("Cart Product IDs:", cartProductIds);
+
+    // Step 2: Use BatchGetCommand to retrieve details of each product in the cart
+    const batchGetParams = {
+      RequestItems: {
+        "Product": {
+          Keys: cartProductIds.map(productId => ({ productID: productId })),
+        },
+      },
+    };
+
+    const batchGetCommand = new BatchGetCommand(batchGetParams);
+    const batchGetResult = await dynamoDB.send(batchGetCommand);
+
+    console.log("Batch Get Result:", batchGetResult);
+
+    // Extract product details from the batchGetResult
+    const Products = batchGetResult.Responses.Product || [];
+
+    console.log("Products:", Products);
+
+    // Pass the product details to the cart.ejs template
     res.render('cart.ejs', { Products: Products });
+
+  } catch (error) {
+    console.error("Error fetching cart data:", error);
+
+    // If there's an error, pass an empty array to Products
+    res.render('cart.ejs', { Products: [] });
+  }
 });
 
 
